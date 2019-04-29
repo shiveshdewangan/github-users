3c1ced5f666498af5ce53b297fb1e0da7756f98a
Shivesh Dewangan <shivesh.dewangan@gmail.com>
2019-04-29 22:38:00 +1000

    refactoring and api requests

---
+++ src/test.jsx
@@ -0,0 +1,63 @@
+    // <div className="tc">
+    //   <h1 className="tc">Github Users</h1>
+    //   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 block">
+    //     <img
+    //       src={`https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg`}
+    //       alt="robots"
+    //     />
+    //     <div>
+    //       <h2>Name</h2>
+    //       <p>Email</p>
+    //     </div>
+    //   </div>
+    //   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 block">
+    //     <img
+    //       src={`https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg`}
+    //       alt="robots"
+    //     />
+    //     <div>
+    //       <h2>Name</h2>
+    //       <p>Email</p>
+    //     </div>
+    //   </div>
+    //   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 block">
+    //     <img
+    //       src={`https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg`}
+    //       alt="robots"
+    //     />
+    //     <div>
+    //       <h2>Name</h2>
+    //       <p>Email</p>
+    //     </div>
+    //   </div>
+    //   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 block">
+    //     <img
+    //       src={`https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg`}
+    //       alt="robots"
+    //     />
+    //     <div>
+    //       <h2>Name</h2>
+    //       <p>Email</p>
+    //     </div>
+    //   </div>
+    //   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 block">
+    //     <img
+    //       src={`https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg`}
+    //       alt="robots"
+    //     />
+    //     <div>
+    //       <h2>Name</h2>
+    //       <p>Email</p>
+    //     </div>
+    //   </div>
+    //   <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 block">
+    //     <img
+    //       src={`https://images.dog.ceo/breeds/chihuahua/n02085620_10131.jpg`}
+    //       alt="robots"
+    //     />
+    //     <div>
+    //       <h2>Name</h2>
+    //       <p>Email</p>
+    //     </div>
+    //   </div>
+    // </div>
